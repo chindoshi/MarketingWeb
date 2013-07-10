551d4002e42114ada23c34d37074cb24c0d7df09
Chintan Doshi <chintan.doshi@fleetnext.com>
2013-07-10 19:31:32 +0530

    pricing plan 250. changed some pricing faq text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
 
 	def format_currency(country)
-		if country.eql?("India") or country.blank?
+		if country.eql?("India")
 			"Rs."
 		else
 			"$"
 		end		
 	end
 
-	def calc_price(cost,country)
-		if country.eql?("India") or country.blank?
-			number_with_delimiter(cost)   
+	def calc_price(inr_cost,usd_cost,country)
+		if country.eql?("India")
+			number_with_delimiter(inr_cost)   
 		else
-			number_with_delimiter(cost/50)   
+			number_with_delimiter(usd_cost)   
 		end	
 	end
 end
